3868d9e770922c748d312d609fc3273876db4db4
Swatantra <102201952+codelikeblueberry@users.noreply.github.com>
2024-04-21 22:29:52 +0530

    added template (groupchat)

--- gzeneric2/myapp/consumers.py
+++ gzeneric2/myapp/consumers.py
@@ -1,31 +1,102 @@
 from channels.generic.websocket import WebsocketConsumer,AsyncWebsocketConsumer
-from asyncio import sleep as asleep
-from time import sleep
+from asgiref.sync import async_to_sync
+import json
+
 class MyWSconsumer(WebsocketConsumer):
     def connect(self):
+        channel_name = self.channel_name  # 1
+        kwargs = self.scope.get("url_route").get("kwargs")
+        self.group_name = kwargs.get("group_name")  # 2
+        scope = self.scope
+        """
+            channel scope {'type': 'websocket', 'path': '/ws/gwc/in/12/', 'raw_path': b'/ws/gwc/in/12/', 'headers': [(b'sec-websocket-version', b'13'), (b'sec-websocket-key', b'SMpj+yx4upqPy7xmwp4j8g=='), (b'connection', b'Upgrade'), (b'upgrade', b'websocket'), (b'sec-websocket-extensions', b'permessage-deflate; client_max_window_bits'), (b'host', b'127.0.0.1:8000')], 'query_string': b'', 'client': ['127.0.0.1', 57701], 'server': ['127.0.0.1', 8000], 'subprotocols': [], 'asgi': {'version': '3.0'}, 'path_remaining': '', 'url_route': {'args': (), 'kwargs': {'groupname': 'in', 'number': 12}}}
+        """
+        print("channel name",channel_name)
+        print("channel group name",self.group_name)
+        async_to_sync(self.channel_layer.group_add)(
+            self.group_name,
+            channel_name
+        )
+
+        print(self.groups)  # RedisChannelLayer(hosts=[{'address': ('localhost', 6379)}])
+        
+
         return super().connect()
     
     def receive(self, text_data=None, bytes_data=None):
-
-        for i in range(10):
-            sleep(1)
-            self.send(text_data=str(i))
-
-        # return super().receive(text_data, bytes_data)
+        message_dict = json.loads(text_data)
+        async_to_sync(self.channel_layer.group_send)(
+            self.group_name,
+            {
+                "type":"message.send",
+                "message":message_dict.get("msg")
+            }
+        )
+        return super().receive(text_data, bytes_data)
     
+    def message_send(self,event):
+        self.send(
+            text_data=json.dumps(
+                {
+                    "msg":event.get("message")
+                }
+            )
+            )
+        print(event)  # {'type': 'message.send', 'message': 'Hi I u are using postman'}
+     
     def disconnect(self, code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.group_name,
+            self.channel_name
+        )
         return super().disconnect(code)
 
 
 class MyAWSconsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        channel_name = self.channel_name  # 1
+        kwargs = self.scope.get("url_route").get("kwargs")
+        self.group_name = kwargs.get("group_name")  # 2
+        scope = self.scope
+        """
+            channel scope {'type': 'websocket', 'path': '/ws/gwc/in/12/', 'raw_path': b'/ws/gwc/in/12/', 'headers': [(b'sec-websocket-version', b'13'), (b'sec-websocket-key', b'SMpj+yx4upqPy7xmwp4j8g=='), (b'connection', b'Upgrade'), (b'upgrade', b'websocket'), (b'sec-websocket-extensions', b'permessage-deflate; client_max_window_bits'), (b'host', b'127.0.0.1:8000')], 'query_string': b'', 'client': ['127.0.0.1', 57701], 'server': ['127.0.0.1', 8000], 'subprotocols': [], 'asgi': {'version': '3.0'}, 'path_remaining': '', 'url_route': {'args': (), 'kwargs': {'groupname': 'in', 'number': 12}}}
+        """
+        print("channel name",channel_name)
+        print("channel group name",self.group_name)
+        await self.channel_layer.group_add(
+            self.group_name,
+            channel_name
+        )
+
+        print(self.groups)  # RedisChannelLayer(hosts=[{'address': ('localhost', 6379)}])
+        
+
         return await super().connect()
     
     async def receive(self, text_data=None, bytes_data=None):
-        for i in range(10):
-            await asleep(1)
-            await self.send(text_data=str(i))
+        message_dict = json.loads(text_data)
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                "type":"message.send",
+                "message":message_dict.get("msg")
+            }
+        )
         return await super().receive(text_data, bytes_data)
     
+    async def message_send(self,event):
+        await self.send(
+            text_data= json.dumps(
+                {
+                    "msg":event.get("message")
+                }
+            )
+            )
+        print(event)  # {'type': 'message.send', 'message': 'Hi I u are using postman'}
+    
     async def disconnect(self, code):
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
         return await super().disconnect(code)
--- gzeneric2/myapp/routes.py
+++ gzeneric2/myapp/routes.py
@@ -2,6 +2,6 @@ from django.urls import path
 from myapp.consumers import MyAWSconsumer,MyWSconsumer
 
 websocket_urlpatterns = [
-    path("ws/gwc/",MyWSconsumer.as_asgi(),name="gwc"),
-    path("ws/gawc/",MyAWSconsumer.as_asgi(),name="gawc"),
+    path("ws/gwc/<str:group_name>/",MyWSconsumer.as_asgi(),name="gwc"),
+    path("ws/gawc/<str:group_name>/",MyAWSconsumer.as_asgi(),name="gawc"),
 ]
